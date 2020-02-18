aab95bd96d1a8d356ec8fdfce1c50e6d24073b33
Emil Stojanovic <emil@horisen.com>
2020-02-18 16:46:34 +0100

    19 started

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
-    require('autoprefixer'),
-
+    require('autoprefixer')
 ]
 
 module.exports = {
